d15220563ed2bf6930c1454509909cc5bd6097d1
Bahram2006 <yardam034@gmail.com>
2026-04-21 11:54:09 +0500

    chore: setup professional proect structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind IS WORKING 🚀
+      </h1>
+    </div>
+  );
+}
