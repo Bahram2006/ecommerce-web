e747d8b7aaab58b61247b930a1f87e2121476cee
Bahram2006 <yardam034@gmail.com>
2026-04-23 09:30:57 +0500

    feat: featureproduct card update fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
 import Hero from "./components/Hero";
+import FeaturedProducts from "./components/product/FeaturedProducts";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <FeaturedProducts />
 
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/components/product/FeaturedProducts.jsx
@@ -0,0 +1,14 @@
+export default function FeaturedProducts() {
+  return (
+    <section className="py-12 bg-white">
+      <div className="max-w-7xl mx-auto px-8">
+
+        {/* TITLE */}
+        <h2 className="text-2xl font-bold mb-6">
+          Featured Products
+        </h2>
+
+      </div>
+    </section>
+  );
+}
